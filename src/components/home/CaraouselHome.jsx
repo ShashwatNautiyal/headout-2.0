33587a6d51df530ed04ab53c53008f4cf9c0373c
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2022-10-16 12:18:31 +0530

    feat: add css effects

--- src/components/home/CaraouselHome.jsx
+++ src/components/home/CaraouselHome.jsx
@@ -1,71 +1,84 @@
-import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import GoogleMap from '../home/GoogleMap';
+import Link from 'next/link';
 
 const images = [
-    {
-        url: "https://images.unsplash.com/photo-1552260300-5dadefa40269?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-        packageSrc: "https://www.headout.com/london-eye-tickets/standard-experience-tickets-to-the-london-eye-e-3336/",
-        caption: "Best Things To Do In London!"
-    },
-    {
-        url: "https://images.thrillophilia.com/image/upload/s--uCLdCIz---/c_fill,h_600,q_auto,w_975/f_auto,fl_strip_profile/v1/images/photos/000/111/500/original/1597302172_shutterstock_1218573694.jpg.jpg",
-        packageSrc: "https://www.headout.com/skydive-sydney-tickets/tandem-skydive-wollongong-15000-ft-e-10351/",
-        caption: "Best Things To Do In Dubai!"
-    },
+  {
+    url: 'https://images.unsplash.com/photo-1552260300-5dadefa40269?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    packageSrc:
+      'https://www.headout.com/london-eye-tickets/standard-experience-tickets-to-the-london-eye-e-3336/',
+    caption: 'Best Things To Do In London!'
+  },
+  {
+    url: 'https://images.thrillophilia.com/image/upload/s--uCLdCIz---/c_fill,h_600,q_auto,w_975/f_auto,fl_strip_profile/v1/images/photos/000/111/500/original/1597302172_shutterstock_1218573694.jpg.jpg',
+    packageSrc:
+      'https://www.headout.com/skydive-sydney-tickets/tandem-skydive-wollongong-15000-ft-e-10351/',
+    caption: 'Best Things To Do In Dubai!'
+  },
 
-    {
-        url: "https://images.unsplash.com/photo-1605443791607-80a259dd3c3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        packageSrc: "headout.com/paris-disneyland-tickets/disneyland-paris-1-day-ticket-e-10209/",
-        caption: "Best Things To Do In Paris!"
-    },
-    {
-        url: "https://images.unsplash.com/photo-1508597370841-836e72ef6f54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
-        packageSrc: "https://www.headout.com/gardens-by-the-bay-tickets/gardens-by-the-bay-direct-entry-tickets-e-7358/",
-        caption: "Best Things To Do In Singapore!"
-
-    },
+  {
+    url: 'https://images.unsplash.com/photo-1605443791607-80a259dd3c3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    packageSrc: 'headout.com/paris-disneyland-tickets/disneyland-paris-1-day-ticket-e-10209/',
+    caption: 'Best Things To Do In Paris!'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1508597370841-836e72ef6f54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+    packageSrc:
+      'https://www.headout.com/gardens-by-the-bay-tickets/gardens-by-the-bay-direct-entry-tickets-e-7358/',
+    caption: 'Best Things To Do In Singapore!'
+  }
 ];
 
 const imgStyle = {
-    width: "100%",
-    height: "550px"
+  width: '100%',
+  height: '550px'
 };
 
 const style = {
-    marginLeft: "60px",
-    top: "10%",
-    height: "40%",
-    width: "25%",
-    color: "white",
-    position: "absolute",
-    fontWeight: "bold",
-    fontSize: "3em",
-    textAlign: "left",
-}
+  marginLeft: '60px',
+  top: '10%',
+  height: '40%',
+  width: '25%',
+  color: 'white',
+  position: 'absolute',
+  fontWeight: 'bold',
+  fontSize: '3em',
+  textAlign: 'left'
+};
 
 const CaraouselHome = () => {
-    return (
-        <Carousel swipeable={true} showArrows={true} autoPlay={false} showThumbs={false} showStatus={false} showIndicators={true} infiniteLoop={true}>
-            {
-                images.map((image, index) => {
-                    return (
-                        <>
-                            <div key={index} >
-                                <img src={image.url} style={imgStyle} />
-                                <a className="legend" style={{ width: "15%", fontSize: "0.8em" }} href={image.packageSrc}>View Package</a>
-                            </div>
-                            <div style={style}>
-                                <p>{image.caption}</p>
-                            </div>
-                        </>
-                    )
-                })
-            }
-            <GoogleMap />
-        </Carousel>
-    );
-}
+  return (
+    <Carousel
+      swipeable={true}
+      showArrows={true}
+      autoPlay={false}
+      showThumbs={false}
+      showStatus={false}
+      showIndicators={true}
+      infiniteLoop={true}
+    >
+      {images.map((image, index) => {
+        return (
+          <>
+            <div key={index}>
+              <img src={image.url} style={imgStyle} />
+              <Link href={'/packages'}>
+                <a className='legend' style={{ width: '15%', fontSize: '0.8em' }}>
+                  View Package
+                </a>
+              </Link>
+            </div>
+            <div style={style}>
+              <p>{image.caption}</p>
+            </div>
+          </>
+        );
+      })}
+      <GoogleMap />
+    </Carousel>
+  );
+};
 
 export default CaraouselHome;
--- src/components/review/index.tsx
+++ src/components/review/index.tsx
@@ -1,58 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { Search } from '../../utils/types/search'
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { Search } from '../../utils/types/search';
 import Image from '../Image';
-import style from "./reviews.module.css"
-import Star from './star'
+import style from './reviews.module.css';
+import Star from './star';
 
 const UNSPLASH_API_KEY = 'pRrZXfPJRI3fi3Bo5s8gwJz7VGgaRh3ZAGapWAaO2vA';
 const Review = (props: any) => {
-  const { review } = props
-  const { profile_image, profile_name, review: reviewText,location } = review
-  const [data, setData] = useState<Search | undefined>(undefined)
-  
-  useEffect(() => {
-    const fetchData=async ()=>{
-      const result = await fetch(`https://api.unsplash.com/search/photos?per_page=3&query=${location ?? 'india'}&client_id=${UNSPLASH_API_KEY}`).then((res) => res.json());
-      setData(result)
-    }
-    fetchData()
-  }, [])
-  
+  const { review } = props;
+  const { profile_image, profile_name, review: reviewText, location } = review;
+  const [data, setData] = useState<Search | undefined>(undefined);
 
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        `https://api.unsplash.com/search/photos?per_page=3&query=${
+          location ?? 'india'
+        }&client_id=${UNSPLASH_API_KEY}`
+      ).then((res) => res.json());
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
   return (
-      <div className='my-16'>
-          <div className={style.profile}>
-              <div className={style.profile__image}>
-                <img
-                    src={profile_image}
-                    alt='profile-pic'
-                    className='h-[40px] w-[40px] border rounded-full object-cover'
-                  />
-              </div>
-              <div className={style.profile__name}>
-                <h3>{profile_name}</h3>
-                  <div className={style.profile__rating}>
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
-                      <Star />
-                </div>
+    <div className='my-16'>
+      <div className={style.profile}>
+        <div className={style.profile__image}>
+          <img
+            src={profile_image}
+            alt='profile-pic'
+            className='h-[40px] w-[40px] border rounded-full object-cover'
+          />
         </div>
+        <div className={style.profile__name}>
+          <h3>{profile_name}</h3>
+          <div className={style.profile__rating}>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
           </div>
-          <div className={style.review}>
+        </div>
+      </div>
+      <div className={style.review}>
         {reviewText}
         <div className='grid grid-cols-3 gap-3 mt-7'>
-          {
-          data?.results.map((image, idx) => (
-            <Image key={idx} alt='img' src={image.urls.small} loading='eager' imageCustomStyles={{width:300,height:200}} />
-          ))
-        }
+          {data?.results.map((image, idx) => (
+            <Link key={idx} href={`/gallery?location=${location}`}>
+              <Image
+                alt='img'
+                src={image.urls.small}
+                loading='eager'
+                className='hover:scale-110 transition-transform duration-300 cursor-pointer'
+                imageCustomStyles={{ width: 300, height: 200 }}
+              />
+            </Link>
+          ))}
         </div>
-            </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
